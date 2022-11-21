8130968cf20b0b5f8279b9183775ba203e9479f1
Lorenzo Montero <lorenzomontero@Lorenzos-MacBook-Air.local>
2022-11-21 13:20:25 -0500

    Removed logo.svg

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-
 const Navbar = () => {
     return (
         <div className="fixed top-0 left-0 h-screen w-32 m-0
-                        flex flex-col bg-neutral-800 rounded-r-lg">
+                        flex flex-col bg-neutral-800 rounded-r-lg-">
 
-            <div>{logo}</div>
+            <div className="">
+                
+            </div>
             <i>B</i>
             <i>C</i>
             <i>D</i>

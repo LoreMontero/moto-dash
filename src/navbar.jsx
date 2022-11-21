8a17602b15ac9371b43afc56c0b2133d1c949968
Lorenzo Montero <lorenzomontero@Lorenzos-MacBook-Air.local>
2022-11-21 11:09:13 -0500

    Moved export default from the top to the bottom.

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,5 +1,15 @@
-export default Navbar = () => {
+const Navbar = () => {
     return (
-        <div className="flex"></div>
+        <div className="fixed top-0 left-0 h-screen w-16 m-0
+                        flex flex-col bg-gray-900">
+
+            <i>A</i>
+            <i>B</i>
+            <i>C</i>
+            <i>D</i>
+            <i>E</i>
+        </div>
     );
 }
+
+export default Navbar;

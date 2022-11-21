fd5485eac8e39a617c3ee9236389eb2439ae47eb
Lorenzo Montero <lorenzomontero@Lorenzos-MacBook-Air.local>
2022-11-21 10:39:09 -0500

    Created a navbar component file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import navbar from './navbar'
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello World
-    </h1>
-  )
+    <div className="flex">
+      <      
+    </div>
+  );
 }

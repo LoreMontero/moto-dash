729414dd99fc126d33141eb275920f58a3ebbbee
Lorenzo Montero <lorenzomontero@Lorenzos-MacBook-Air.local>
2022-11-21 19:59:21 -0500

    Just created Tables.jsx component file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import Navbar from './Navbar';
+import Navbar from './components/Navbar';
 
 export default function App() {
   return (
     <div className="flex h-screen bg-[radial-gradient(ellipse_at_top_right,_var(--tw-gradient-stops))] from-indigo-900 via-blue-400 to-pink-400">
         <Navbar />
- 
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,29 @@
-import logo from './logo.svg';
+import logo from '../logo.svg';
 import { EnvelopeIcon, Squares2X2Icon, CalendarIcon, RectangleStackIcon  } from '@heroicons/react/24/solid';
 
 const Navbar = () => {
     return (
         <div className="fixed top-0 left-0 h-screen w-36 m-0
-                        flex flex-col bg-black
-                        bg-opacity-60 backdrop-filter gap-20">
+                        flex flex-col bg-black bg-opacity-60 
+                        backdrop-filter gap-20">
 
             <div className="flex justify-center w-32 mt-4">
                 <img src={logo} alt="Motorsport Logo" />
             </div>
             <div className="flex flex-col gap-4">
-                <div className="flex justify-center text-white hover:bg-sky-500 rounded-2xl duration-300">
+                <div className="flex justify-center text-white hover:bg-sky-500 rounded-2xl duration-300 p-2">
                     <RectangleStackIcon className="w-4"/>
                     <p>Dashboard</p>
                 </div>
-                <div className="flex justify-center text-white hover:bg-sky-500 rounded-2xl">
+                <div className="flex justify-center text-white hover:bg-sky-500 rounded-2xl duration-300 p-2">
                     <Squares2X2Icon className="w-4"/>
                     <p>Workspace</p>
                 </div>
-                <div className="flex justify-center text-white hover:bg-sky-500 rounded-2xl">
+                <div className="flex justify-center text-white hover:bg-sky-500 rounded-2xl duration-300 p-2">
                     <EnvelopeIcon className="w-4"/>
                     <p>Inbox</p>
                 </div>    
-                <div className="flex justify-center text-white hover:bg-sky-500 rounded-2xl">
+                <div className="flex justify-center text-white hover:bg-sky-500 rounded-2xl duration-300 p-2">
                     <CalendarIcon className="w-4"/>
                     <p>Events</p>
                 </div>

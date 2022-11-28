9b2a2e12d0ab0af0e36855a9c3fd81c80e4bbc6d
Lorenzo Montero <lorenzomontero@Lorenzos-MacBook-Air.local>
2022-11-28 14:37:05 -0500

    Added a tables component to App.jsx to see a test how it will look.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import Navbar from './components/Navbar';
+import Tables from './components/Tables'
 
 export default function App() {
   return (
-    <div className="flex h-screen bg-[radial-gradient(ellipse_at_top_right,_var(--tw-gradient-stops))] from-indigo-900 via-blue-400 to-pink-400">
+    <div className="flex justify-evenly h-screen bg-[radial-gradient(ellipse_at_top_right,_var(--tw-gradient-stops))] from-indigo-900 via-blue-400 to-pink-400">
         <Navbar />
+        <div className="flex flex-col">
+          <h1></h1>
+          <div className="flex">
+            <Tables />
+          </div>
+        </div>
     </div>
   );
 }
---
+++ src/components/Tables.jsx
@@ -0,0 +1,34 @@
+const Tables = () => {
+    return (
+        <div>
+            <table class="table-auto text-white rounded-2xl p-6 bg-black bg-opacity-60 backdrop-filter">
+            <thead>
+                <tr>
+                <th>Task</th>
+                <th>Progress</th>
+                <th>Files</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                <td>Brake Calipers</td>
+                <td>Complete</td>
+                <td>empty</td>
+                </tr>
+                <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                </tr>
+                <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                </tr>
+            </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default Tables;

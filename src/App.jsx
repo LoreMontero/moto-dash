e539d9b7ec0a004e0e3692b46f648b456331e42c
Lorenzo Montero <lorenzomontero@Lorenzos-MacBook-Air.local>
2022-12-05 12:19:17 -0500

    Just created a Topbar file for search bar and profile icon.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import Navbar from './global/Navbar';
+import Navbar from './scenes/global/Navbar';
+import { ColorModeContext, useMode } from './theme';
+import { CssBaseLine, ThemeProvider } from '@mui/material';
+import Topbar from './scenes/global/Topbar';
 
 export default function App() {
+  const [theme, colorMode] = useMode();
   return (
-    <div className="flex h-screen bg-[conic-gradient(at_top_right,_var(--tw-gradient-stops))] from-violet-800 via-cyan-100 to-violet-100">
-        <Navbar />
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="flex h-screen bg-[conic-gradient(at_top_right,_var(--tw-gradient-stops))] from-violet-800 via-cyan-100 to-violet-100">
+            <Navbar />
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }

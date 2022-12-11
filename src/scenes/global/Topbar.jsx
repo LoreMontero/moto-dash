2db1a1a6d76c99834540bf25f503afce6476c6f9
Lorenzo Montero <lorenzomontero@Lorenzos-MacBook-Air.local>
2022-12-11 13:48:00 -0500

    Added a search bar and added borders to the search bar and the notifications icon.

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,4 +1,4 @@
-import { MagnifyingGlassIcon, BellIcon } from '@heroicons/react/24/solid';
+import { MagnifyingGlassIcon, BellIcon, ChevronDownIcon } from '@heroicons/react/24/solid';
 
 const Topbar = () => {
     return (
@@ -7,11 +7,22 @@ const Topbar = () => {
                 <h1 className="rubic font-bold">Dashboard</h1>
             </div>
             <div className="flex justify-center items-center">
-                <div className="">
-                    <MagnifyingGlassIcon className="fill-black w-6 "/>
+                <div className="relative p-2 border-solid border-2 border-black rounded-2xl">
+                    <span className="absolute inset-y-0 flex items-center px-3 pointer-events-none">
+                        <MagnifyingGlassIcon className="fill-black w-5 "/> 
+                    </span>
+                    <input 
+                        type="text" 
+                        placeholder="Search" 
+                        className="rounded-10 bg-transparent py3 pr-4 pl-10 text-sm focus:text-black focus:outline-none"
+                    />
+
+                </div>
+                <div className="p-2 border-solid border-2 border-black rounded-2xl hover:bg-blue-600">
+                    <BellIcon className="fill-black w-5"/>
                 </div>
                 <div>
-                    <BellIcon className="fill-black w-6 "/>
+
                 </div>
             </div>
         </div>

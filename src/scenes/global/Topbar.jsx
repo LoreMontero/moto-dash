c83b466e7903aff5ea127d1594399d7f7c96c9cc
Lorenzo Montero <lorenzomontero@Lorenzos-MacBook-Air.local>
2022-12-11 22:29:52 -0500

    Added motorHead jpeg to use as a placeholder for account picture.

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,29 +1,30 @@
-import { MagnifyingGlassIcon, BellIcon, ChevronDownIcon } from '@heroicons/react/24/solid';
+import { MagnifyingGlassIcon, BellIcon } from '@heroicons/react/24/solid';
 
 const Topbar = () => {
     return (
         <div className="flex fixed justify-center items-center mt-0 ml-20 h-16 w-screen">
             <div className="flex justify-center p-5">
-                <h1 className="rubic font-bold">Dashboard</h1>
+                <h1 className="rubic font-bold text-xl">Dashboard</h1>
             </div>
             <div className="flex justify-center items-center">
                 <div className="relative p-2 border-solid border-2 border-black rounded-2xl">
                     <span className="absolute inset-y-0 flex items-center px-3 pointer-events-none">
-                        <MagnifyingGlassIcon className="fill-black w-5 "/> 
+                        <MagnifyingGlassIcon className="fill-black w-5"/> 
                     </span>
                     <input 
                         type="text" 
                         placeholder="Search" 
-                        className="rounded-10 bg-transparent py3 pr-4 pl-10 text-sm focus:text-black focus:outline-none"
+                        className="rounded-10 bg-transparent pr-4 pl-10 text-sm focus:text-black focus:outline-none"
                     />
 
                 </div>
                 <div className="p-2 border-solid border-2 border-black rounded-2xl hover:bg-blue-600">
                     <BellIcon className="fill-black w-5"/>
                 </div>
-                <div>
-
-                </div>
+                <button>
+                    <img src="" alt="" />
+                    
+                </button>
             </div>
         </div>
     );

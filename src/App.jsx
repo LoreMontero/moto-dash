58929dace6670280d08cd7fc827ecd3e89cd5083
Lorenzo Montero <lorenzomontero@Lorenzos-MacBook-Air.local>
2022-12-14 22:04:07 -0500

    Trying to add symlink to import Tables.jsx file into App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Sidebar from './scenes/global/Sidebar';
 import Topbar from './scenes/global/Topbar';
+import Tables from '../components/Tables'
 
 export default function App() {
   return (
     <div className="flex h-screen font-sans bg-[conic-gradient(at_top_right,_var(--tw-gradient-stops))] from-violet-800 via-cyan-100 to-violet-100">
       <Sidebar />
-      <Topbar />
+      <div className="flex flex-column">
+        <Topbar />
+        <Tables />
+      </div>
     </div>
   );
 }

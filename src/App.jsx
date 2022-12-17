d6d3ec8b1a2be5a9143d7ba0c0a2145e97a7710e
Lorenzo Montero <lorenzomontero@Lorenzos-MacBook-Air.local>
2022-12-17 12:59:39 -0500

    Made a change in App.jsx moving the website code to the Dashboard page and using App.jsx to Route the pages making it a multi page website.

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
-import Sidebar from './scenes/global/Sidebar';
-import Topbar from './scenes/global/Topbar';
-import Tables from './components/Tables';
+import React from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import Dashboard from './scenes/Dashboard';
 
 export default function App() {
   return (
-    <div className="flex h-screen font-sans bg-[conic-gradient(at_top_right,_var(--tw-gradient-stops))] from-violet-800 via-cyan-100 to-violet-100">
-      <Sidebar />
-      <div className="flex flex-col h-screen w-screen ml-20">
-        <Topbar />
-        <div className="flex flex-col justify-evenly h-screen">
-          <div className="flex justify-around">
-            <Tables />
-            <Tables />
-          </div>
-          <div className="flex justify-center">
-            <Tables /> 
-          </div> 
-        </div>
-      </div>
-    </div>
+    <Router>
+      <Route exact path="/" component={Dashboard} />      
+    </Router>
   );
 }

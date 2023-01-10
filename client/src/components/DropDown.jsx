c9f184edc96b574d6f09bc3871a24c951948b3ca
Lorenzo Montero <lorenzomontero@Lorenzos-MacBook-Air.local>
2023-01-10 17:44:56 -0500

    Merged the changes.

--- client/src/components/DropDown.jsx
+++ client/src/components/DropDown.jsx
@@ -44,7 +44,6 @@ const DropDown = ({data}) => {
                                             ) : null}
                                         </div>
                                     )}
-
                                 </Listbox.Option>
                             ))}
                         </Listbox.Options>
